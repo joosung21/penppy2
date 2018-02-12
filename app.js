d6b255c52a8068ee35de445bf26519725488ac1d
Joosung Ahn <joosung21@gmail.com>
2018-02-12 16:33:07 +0900

    Init Project

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+var pug = require('pug')
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.use('/', express.static('public'))
+app.use('/assets/', express.static('assets'))
+app.use('/node_modules/', express.static('node_modules'))
+
+app.get('/', function (req, res){res.render('index')})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!')
+})
