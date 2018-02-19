6c680d7318d853f20435d292f54389e7ddd55848
Joosung Ahn <joosung21@gmail.com>
2018-02-19 10:04:08 +0900

    Done Calendar

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ app.use('/', express.static('public'))
 app.use('/assets/', express.static('assets'))
 app.use('/node_modules/', express.static('node_modules'))
 
-app.get('/', function (req, res){res.render('index')})
+app.get('/', function (req, res){res.render('calendar')})
+app.get('/calendar', function (req, res){res.render('calendar')})
+
+
+
 app.get('/chart', function (req, res){res.render('chart')})
+app.get('/test', function (req, res){res.render('test')})
 
 app.listen(3200, function () {
   console.log('App listening on port 3200!')
