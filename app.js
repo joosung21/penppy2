8a596f34df9a9d8180c9b50b38f50c937466ea93
Joosung Ahn <joosung21@gmail.com>
2018-02-21 11:30:01 +0900

    Complete Setting - Daily page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use('/node_modules/', express.static('node_modules'))
 
 app.get('/', function (req, res){res.render('calendar')})
 app.get('/calendar', function (req, res){res.render('calendar')})
+app.get('/calendar-week', function (req, res){res.render('calendar-week')})
 
 // 업체설정
 app.get('/setting/property', function (req, res){res.render('setting-property')})
@@ -17,6 +18,7 @@ app.get('/setting/rooms', function (req, res){res.render('setting-rooms')})
 app.get('/setting/rooms/edit', function (req, res){res.render('setting-room-edit')})
 app.get('/setting/service', function (req, res){res.render('setting-service')})
 app.get('/setting/service/edit', function (req, res){res.render('setting-service-edit')})
+app.get('/setting/daily', function (req, res){res.render('setting-daily')})
 
 //-- Temporary Pages
 app.get('/test', function (req, res){res.render('test')})
