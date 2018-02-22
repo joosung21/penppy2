9575946cd0cf6743056a94e50e4886586e111094
Joosung Ahn <joosung21@gmail.com>
2018-02-22 12:07:55 +0900

    Edit Font Size

--- public/js/penppy2.js
+++ public/js/penppy2.js
@@ -3,6 +3,18 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+// Slider Open and Close
+function openSlider(){
+  $('.slider-ui').removeClass('hidden');
+  $('.slider-ui').addClass('show');
+}
+function closeSlider(){
+  $('.slider-ui').removeClass('show');
+  $('.slider-ui').addClass('hidden');
+  $('.room-line.assign-ui').removeClass('show')
+}
+
+
 
 // Document Ready
 $( document ).ready(function() {
