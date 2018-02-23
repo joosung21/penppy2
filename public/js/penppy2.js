9a83f8deaca965a0bce3768b30a43e6a89be6272
Joosung Ahn <joosung21@gmail.com>
2018-02-23 16:25:30 +0900

    Done Responsive Nav

--- public/js/penppy2.js
+++ public/js/penppy2.js
@@ -38,7 +38,6 @@ $( document ).ready(function() {
   // Show Second Depth Nav
   if ($('body').hasClass('two-depth')) {
     $('#main-nav').addClass('two-depth');
-
     var thisPage = $('body').attr('name');
     switch(thisPage) {
       case 'setting':
@@ -51,5 +50,57 @@ $( document ).ready(function() {
         break;
     }
   }
+
+  // Responsive: Second Depth Show
+  var windowWidth = $(window).width();
+  $( window ).resize(function() {
+    windowWidth = $(window).width();
+    if( windowWidth >= 1200 ) {
+      $('#main-nav').addClass('two-depth');
+      var thisPage = $('body').attr('name');
+      switch(thisPage) {
+        case 'setting':
+          $('#nav-setting').show();
+          break;
+        case 'rateplan':
+          $('#nav-rateplan').show();
+          break;
+        default:
+          break;
+      }
+    }
+    if( windowWidth <= 1199 ) {
+      $('#main-nav.two-depth .second-depth').hide();
+    }
+  });
+
+  if( windowWidth <= 1199 ) {
+    $('#main-nav.two-depth .second-depth').hide();
+    var isOpend1 = false;
+    var isOpend2 = false;
+    $('#main-nav.two-depth .first-depth a').mouseover(function(){
+      if($(this).hasClass('setting')) {
+        if(isOpend1 == false) {
+          $('#main-nav.two-depth .second-depth').hide();
+          $('#main-nav.two-depth #nav-setting').slideDown(300);
+          isOpend1 = true;
+        }
+      }
+      if($(this).hasClass('rateplan')) {
+        if(isOpend2 == false) {
+          $('#main-nav.two-depth .second-depth').hide();
+          $('#main-nav.two-depth #nav-rateplan').slideDown(300);
+          isOpend2 = true;
+        }
+      }
+    });
+    $('#main-nav').mouseleave(function(){
+      $('#main-nav.two-depth .second-depth').slideUp(300);
+      isOpend1 = false;
+      isOpend2 = false;
+    });
+  }
+
+
 
 });
